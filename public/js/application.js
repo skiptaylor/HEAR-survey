e15bea379c11377fc5e8552e04b070bade1a22e1
Skip Taylor <skiptaylor101@gmail.com>
2022-07-29 07:15:47 -0400

    reset fix

--- public/js/application.js
+++ public/js/application.js
@@ -1,21 +1,21 @@
-$(function() {
-
-	$('.link-row').click(function() {
-		window.location = $(this).attr('id');
-	});
-	
-	$('a.job-link[data-status="Closed"]').hide();
-	
-	$('#job-list-toggle button').click(function() {
-		$('#job-list-toggle button').removeClass('active');
-		$(this).addClass('active');
-		var status = $(this).attr('data-toggle');
-		$('a.job-link').hide();
-		$('a.job-link[data-status="' + status + '"]').show();
-		return false;
-	});
-			
-});
+// $(function() {
+//
+// 	$('.link-row').click(function() {
+// 		window.location = $(this).attr('id');
+// 	});
+//
+// 	$('a.job-link[data-status="Closed"]').hide();
+//
+// 	$('#job-list-toggle button').click(function() {
+// 		$('#job-list-toggle button').removeClass('active');
+// 		$(this).addClass('active');
+// 		var status = $(this).attr('data-toggle');
+// 		$('a.job-link').hide();
+// 		$('a.job-link[data-status="' + status + '"]').show();
+// 		return false;
+// 	});
+//
+// });
 
 $(function() {
   $(document).ready(function() {
@@ -37,12 +37,21 @@ $(function() {
   });
 }).call(this);
 
-(function() {
-  $(window).load(function() {
-    return $('a#reset-password').click(function() {
-      return $('a#reset-password').attr('href', "/reset-password/" + ($('input#email').val()));
-    });
+$(window).load(function() {
+  $('a[rel="popover"]').hover(function() {
+    return $(this).popover('toggle');
   });
+  $('div.link').click(function() {
+    if ($(this).attr('target') === '_blank') {
+      return window.open($(this).attr('data-url'));
+    } else {
+      return window.location = $(this).attr('data-url');
+    }
+  });
+  return $('a#reset-password').click(function() {
+    return $('a#reset-password').attr('href', "/reset-password/" + ($('input#email').val()));
+  });
+});
 
 }).call(this);
 
