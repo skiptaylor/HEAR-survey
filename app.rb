a8337a1584ae49038267387b21399196965ed65d
Skip Taylor <skiptaylor101@gmail.com>
2022-11-16 08:50:32 -0500

    Redirects to www

--- app.rb
+++ app.rb
@@ -17,6 +17,14 @@ set :session_secret, 'secret123'
 	Dir["./#{directory}/**/*.rb"].each { |file| require file }
 end
 
-
+configure :production do
+  before do
+    unless request.request_method == 'POST'
+      unless request.url.include? "https://www."
+        redirect "https://www.hearsurvey.com#{request.path}"
+      end
+    end
+  end
+end
 
 DataMapper.finalize
